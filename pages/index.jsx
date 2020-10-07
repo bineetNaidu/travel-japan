cf3e0d1129a8e8150d70be79aab8c028d714c2b6
bineetNaidu <bineet.s.naidu@gmail.com>
2020-10-07 21:43:33 +0530

    add base index Page

--- pages/index.jsx
+++ pages/index.jsx
@@ -1,14 +1,41 @@
 import React from 'react';
 import style from '../styles/index.module.css';
 import Header from '../components/Header';
+import { motion } from 'framer-motion';
+
+// Variants
+const containerVariants = {
+  initial: {
+    opacity: 0,
+  },
+  animate: { opacity: 1, transition: { delay: 1 } },
+};
+
+const tileVariants = {
+  initial: {
+    opacity: 0,
+  },
+  animate: { opacity: 1, transition: { delay: 1 } },
+};
 
 const Index = () => {
   return (
-    <div className={style.index}>
-      <div className={style.darkWrapper}></div>
-      <Header />
-      <h1>Welcome to Japan</h1>
-    </div>
+    <>
+      <div className={style.index}>
+        <Header />
+        <div className={style.darkWrapper}></div>
+        <motion.div
+          variants={containerVariants}
+          initial="initial"
+          animate="animate"
+          className={style.index__wrapper}
+        >
+          <motion.h1 className={style.index__tile} variants={tileVariants}>
+            Welcome to Japan
+          </motion.h1>
+        </motion.div>
+      </div>
+    </>
   );
 };
 
